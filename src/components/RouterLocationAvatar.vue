0a4f9be92399f9ea3717af4cde8ad48ee9b8affc
iEdon <iedonami@gmail.com>
2022-06-15 23:41:12 +0800

    fix: force using lower case for flag

--- src/components/RouterLocationAvatar.vue
+++ src/components/RouterLocationAvatar.vue
@@ -11,12 +11,12 @@ defineProps<{
 <template>
     <template v-if="!hidePeeringDot">
         <a-badge v-if="router" dot :offset="[0, 35]" :numberStyle="{width:'15px',minWidth:'15px',height:'15px'}" :color="`${router.openPeering ? (router.sessionCount < router.sessionCapacity ? (router.autoPeering ? 'green' : 'gold') : 'red') : 'red'}`">
-            <img v-if="iso31661Alpha2ToNumeric[router.location] !== undefined" :src="`flags/${router.location}.svg`" width="48" class="country-flag" />
+            <img v-if="iso31661Alpha2ToNumeric[router.location] !== undefined" :src="`flags/${router.location.toLowerCase()}.svg`" width="48" class="country-flag" />
             <div shape="square" class="text-avatar" v-else-if="router.location"><span>{{ router.location.length > 3 ? router.location.substring(0, 3) : router.location }}</span></div>
         </a-badge>
     </template>
     <template v-else-if="router">
-        <img v-if="iso31661Alpha2ToNumeric[router.location] !== undefined" :src="`flags/${router.location}.svg`" width="48" class="country-flag" />
+        <img v-if="iso31661Alpha2ToNumeric[router.location] !== undefined" :src="`flags/${router.location.toLowerCase()}.svg`" width="48" class="country-flag" />
         <div shape="square" class="text-avatar" v-else-if="router.location"><span>{{ router.location.length > 3 ? router.location.substring(0, 3) : router.location }}</span></div>
     </template>
 </template>
