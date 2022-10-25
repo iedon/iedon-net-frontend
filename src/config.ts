afe7facc2968e961b833d05d2bcea27a209f9baa
iEdon <m@iedon.net>
2022-10-25 17:38:00 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import pkg from '../package.json'
 export default {
     root: '/',
     package: `${pkg.name}/${pkg.version}`,
-    apiPrefix: 'https://api.kuu.moe/service/dn42',
+    apiPrefix: 'https://api.iedon.com/service/dn42',
     pingIntervalMs: 60000,
     configFallback: {
         netAsn: '4242422189',
