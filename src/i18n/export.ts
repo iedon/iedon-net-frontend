c8b997c244a417ec9849bc27d3f7a6c6db77745b
iEdon <iedonami@gmail.com>
2024-11-17 14:06:50 +0900

    feat: translation: add French

--- src/i18n/export.ts
+++ src/i18n/export.ts
@@ -3,11 +3,13 @@ import ja_JP from './ja_JP'
 import zh_TW from './zh_TW'
 import zh_CN from './zh_CN'
 import de_DE from './de_DE'
+import fr_FR from './fr_FR'
 
 export default {
     en_US,
     ja_JP,
     zh_TW,
     zh_CN,
-    de_DE
+    de_DE,
+    fr_FR
 }
