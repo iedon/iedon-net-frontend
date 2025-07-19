eabe982dba64726aac79ca9c8ff64ca229840ec9
iEdon <iedonami@gmail.com>
2025-07-19 16:53:04 +0900

    chore: add us-nyc and uk-lon looking glass

--- src/config.ts
+++ src/config.ts
@@ -35,5 +35,7 @@ export default {
       'b2754a85-a4fe-46c0-a392-1988a548b688': 'https://lg.iedon.net/detail/de-fra/',
       'c88b0ffa-d9f7-4672-86e6-064b4b68aa05': 'https://lg.iedon.net/detail/jp-tyo/',
       'ca5196a1-ab8d-430d-a918-b386ad5c1632': 'https://lg.iedon.net/detail/cn-czx/',
+      'a916ec9a-d4ce-4b8a-82ce-692a3fc44779': 'https://lg.iedon.net/detail/uk-lon/',
+      'a331daff-40bd-478c-ac0d-a7b14bbcbaae': 'https://lg.iedon.net/detail/us-nyc/',
     }
 }
